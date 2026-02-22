4c940c00d61e3a3c16acb83d5104b12d38536583
Alking <alrey2635@gmail.com>
2026-02-22 20:03:44 +0300

    first commit

---
+++ src/main/java/com/alpha/push_down_td7/PushDownTd7Application.java
@@ -0,0 +1,13 @@
+package com.alpha.push_down_td7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushDownTd7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushDownTd7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpha/push_down_td7/PushDownTd7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.push_down_td7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushDownTd7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
