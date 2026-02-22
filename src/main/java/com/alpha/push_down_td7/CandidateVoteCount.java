a1de818f230adea39b7db46d79a813aff563bbcf
Alking <alrey2635@gmail.com>
2026-02-22 20:57:14 +0300

    added the class candidateVoteCount

--- src/main/java/com/alpha/push_down_td7/CandidateVoteCount.java
+++ src/main/java/com/alpha/push_down_td7/CandidateVoteCount.java
@@ -1,4 +1,19 @@
 package com.alpha.push_down_td7;
 
 public class CandidateVoteCount {
+    private String candidateName;
+    private long validVoteCount;
+
+    public CandidateVoteCount(String candidateName, long validVoteCount) {
+        this.candidateName = candidateName;
+        this.validVoteCount = validVoteCount;
+    }
+
+    public String getCandidateName() { return candidateName; }
+    public long getValidVoteCount() { return validVoteCount; }
+
+    @Override
+    public String toString() {
+        return candidateName + "=" + validVoteCount;
+    }
 }
