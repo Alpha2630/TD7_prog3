97e8512aae1e853eb5276b9af846eeb165b04f65
Alking <alrey2635@gmail.com>
2026-02-24 09:45:29 +0300

    Added the method that calculates the total number of votes

--- src/main/java/com/alpha/push_down_td7/DataRetriever.java
+++ src/main/java/com/alpha/push_down_td7/DataRetriever.java
@@ -1,4 +1,28 @@
 package com.alpha.push_down_td7;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataRetriever {
+    private final DatabaseConnection dbConnection;
+
+    public DataRetriever(DatabaseConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
+    // Q1 - Nombre total de votes
+    public long countAllVotes() throws SQLException {
+        String sql = "SELECT COUNT(id) as total_votes FROM vote";
+
+        Connection conn = dbConnection.getConnection();
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            if (rs.next()) {
+                return rs.getLong("total_votes");
+            }
+            return 0L;
+        }
+    }
 }
